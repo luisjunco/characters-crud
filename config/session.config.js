9f9ce4f677b220d897291c1dd21d22a95a20572f
luisjunco <38411763+luisjunco@users.noreply.github.com>
2022-07-14 11:57:54 +0200

    Update session.config.js

--- config/session.config.js
+++ config/session.config.js
@@ -21,11 +21,11 @@ module.exports = app => {
         sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
         secure: process.env.NODE_ENV === 'production',
         httpOnly: true,
-        maxAge: 60000
+        maxAge: 1000 * 60 * 60 * 24 // 24h
       },
       store: MongoStore.create({
         mongoUrl: process.env.MONGODB_URI || 'mongodb://localhost/characters-crud',
-        ttl: 60 * 60 // 60sec * 60min * 24h => 1 day
+        ttl: 60 * 60 * 24 // 60sec * 60min * 24h => 1 day
       })
     })
   );
